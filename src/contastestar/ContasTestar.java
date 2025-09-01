c4d319a0bcc20b766c87aebd844c2d5706f24bbb
Raul Guilherme <raulawp460@gmail.com>
2025-09-01 01:09:21 -0300

    sobreposição e anotação override

--- src/contastestar/ContasTestar.java
+++ src/contastestar/ContasTestar.java
@@ -8,7 +8,20 @@ import entities.SavingsAccount;
 public class ContasTestar {
 
     public static void main(String[] args) {
-       BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+       
+       Account acc1 = new Account(1001, "Alex", 1000.0);
+       acc1.withdraw(200.0);
+       System.out.println(acc1.getBalance());
+        
+       Account acc2 = new SavingsAccount(1002, "Marcos", 1000.0, 0.01);
+       acc2.withdraw(200.0);
+       System.out.println(acc2.getBalance());
+       
+       Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+       acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+        
+       /* BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
        Account acc = new Account(1001, "Alex", 0.0);
        
        //UPCASTING
@@ -30,7 +43,7 @@ public class ContasTestar {
            SavingsAccount acc5 = (SavingsAccount)acc3;
            acc5.updateBalance(200.0);
            System.out.println("Update!");
-       }
+       } */
        
     }
     
--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -30,4 +30,10 @@ public class SavingsAccount extends Account {
         balance += balance * interestRate;
     }
     
+    @Override
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+    
+    
 }
